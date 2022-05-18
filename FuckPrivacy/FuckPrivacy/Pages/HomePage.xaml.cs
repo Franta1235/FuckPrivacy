10d20e4d0becc867f78fffa46d1e484c1a131731
Franta <koznar.gmail@gmail.com>
2022-05-18 10:56:24 +0200

    HomePage

--- FuckPrivacy/FuckPrivacy/Pages/HomePage.xaml.cs
+++ FuckPrivacy/FuckPrivacy/Pages/HomePage.xaml.cs
@@ -11,6 +11,12 @@ using Xamarin.Forms.Xaml;
 
 namespace FuckPrivacy.Pages
 {
+    public class Photo
+    {
+        public string Image { get; set; }
+        public string Text { get; set; }
+    }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
@@ -23,36 +29,13 @@ namespace FuckPrivacy.Pages
             var vm = new HomePageViewModel();
             BindingContext = vm;
 
+            var photos = new ObservableCollection<Photo>();
+            for (var i = 0; i < 100; i++) {
+                photos.Add(new Photo() {Image = "beerIcon", Text = $"{i.ToString()}"});
+            }
 
-            var stackLayout = new StackLayout() {
-                Padding = new Thickness(10, 10, 0, 0),
-                Children = {
-                    new Frame() {
-                        CornerRadius = 5,
-                        Padding = 1,
-                        Content = new StackLayout() {
-                            Orientation = StackOrientation.Horizontal,
-                            Children = {
-                                new Image() {
-                                    Source = "beerIcon",
-                                    HeightRequest = 60,
-                                    Margin = new Thickness(10, 0, 10, 0)
-                                },
-                                new Label() {
-                                    Text = "My Text",
-                                    FontAttributes = FontAttributes.Bold,
-                                    Margin = new Thickness(0, 20, 10, 0)
-                                }
-                            }
-                        }
-                    }
-                }
-            };
 
-            var images = new ObservableCollection<StackLayout>();
-            images.Add(stackLayout);
-            
-            Photos.ItemsSource = images;
+            Photos.ItemsSource = photos;
         }
     }
 }
