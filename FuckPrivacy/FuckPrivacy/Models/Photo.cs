6a6f5732abd55edbb90cb259f16df9ffb88a1984
Franta <koznar.gmail@gmail.com>
2022-05-20 16:02:01 +0200

    users

--- FuckPrivacy/FuckPrivacy/Models/Photo.cs
+++ FuckPrivacy/FuckPrivacy/Models/Photo.cs
@@ -1,10 +1,15 @@
-﻿namespace FuckPrivacy.Models
+﻿using FuckPrivacy.Users;
+using Newtonsoft.Json;
+
+namespace FuckPrivacy.Models
 {
     public class Photo
     {
-        public string Image { get; set; }
-        public string Text { get; set; }
-
-        public string Caption { get; set; }
+        [JsonProperty("ImageSource")] public string Image { get; set; }
+        [JsonProperty("Text")] public string Text { get; set; }
+        [JsonProperty("Caption")] public string Caption { get; set; }
+        [JsonProperty("UserFrom")] public AUser UserFrom { get; set; }
+        [JsonProperty("UserTo")] public AUser UserTo { get; set; }
+        [JsonProperty("Time")] public string Time { get; set; }
     }
 }
---
+++ Server/Servers/MySQL/users.sql
@@ -0,0 +1,7 @@
+﻿-- auto-generated definition
+create table users
+(
+    username text not null,
+    password text not null
+)
+    engine = InnoDB;
