851308c4e4440ddf2573f3e02eb081dfc1f05a6b
Franta <koznar.gmail@gmail.com>
2022-05-20 16:05:25 +0200

    photos.sql

---
+++ Server/Servers/MySQL/photos.sql
@@ -0,0 +1,8 @@
+﻿create table photos
+(
+    source    text     not null,
+    caption   text null,
+    user_from text     not null,
+    user_to   text     not null,
+    time      datetime not null
+);
